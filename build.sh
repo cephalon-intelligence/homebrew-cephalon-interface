de1f4eec2f2495b44d289baf70a077d768b0a3fa
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-12-12 20:31:35 -0500

    ,

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ if [ -d "./.venv" ]; then
     rm -rf "./.venv"
 fi
 
+if [ -d "./Formula/cephalon-interface.rb" ]; then
+    rm "./Formula/cephalon-interface.rb"
+fi
+
 virtualenv .venv
 source .venv/bin/activate
 pip install cephalon-interface homebrew-pypi-poet
